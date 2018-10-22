35694f101093be4cd0198ea98c2212089397785a
Hussain Haider <hussainhaider490@gmail.com>
2018-10-22 13:40:40 +0500

    Files

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,182 +1,121 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-
-public class Ball : MonoBehaviour {
+public class Ball : MonoBehaviour
+    {
 
 
     public TextMesh potential_energy;
     public TextMesh kinetic_energy;
 
+
+
     private Rigidbody rb;
-    private float mass;
-    private float velocity;
-    private float height;
+    public static float mass = 1;
+
+
+    public float velocity;
+    public static float height =1;
 
     private float KE;
     private float PE;
-    private float PE_max;
-    private float PE_min;
-    private float PE_avg;
-    private float gravitinal_force;
+    public static float gravitinal_force = 9.8f;
 
-    public GameObject[] PE_Cubes;
-    public GameObject[] KE_Cubes;
 
     float time = 5.00f;
     bool flag = false;
     float value = 0.005f;
     float YValue;
     Vector3 previous;
-    int count1 = 5;
-    int count2 = 0;
-    float[] arr;
 
-    void Start () {
+    float ballheightupto;
+
+    void Awake()
+    {
 
         YValue = 0;
-        arr = new float[6];
 
         rb = GetComponent<Rigidbody>();
         mass = rb.mass;
+        //Debug.Log("---Mass is ---:" + mass);
 
-        kinetic_energy.text = "Kinetic Energy =";
-        potential_energy.text = "Potential Energy = ";
-        gravitinal_force = 9.8f;
-        for (int i = 0; i < PE_Cubes.Length; i++)
-        {
-            PE_Cubes[i].GetComponent<Renderer>().material.color = Color.green;
-        }
-        for (int i = 0; i < KE_Cubes.Length; i++)
-        {
-            KE_Cubes[i].GetComponent<Renderer>().material.color = Color.white;
-        }
+        //kinetic_energy.text = "Kinetic Energy =";
+        //potential_energy.text = "Potential Energy = ";
 
-        PE_max = this.transform.localPosition.y;
-        
-        float pvalue = PE_max / 6;
+        ballheightupto = this.transform.localPosition.y;
 
-        for (int i = 0; i < 6; i++)
-        {
-            arr[i] = (i + 1) * pvalue;
-            //Debug.Log("arr: "+"i= " + arr[i]);
-        }
-
-
-
-        PE_avg = (PE_max + PE_min) / 2;
     }
 
-    int determineCount(float YValue)
-    {
-        int count = 0;
-        for (int i = 0; i < 6; i++)
-        {
-            if (YValue >= arr[i])
-            {
-                count = i;
-            }
-        }
-        return count;
-    }
 
     void FixedUpdate()
-    {
-        if (time >= 0)
         {
-            time -= Time.deltaTime;
-            return;
-        }
-        else
-        {
-            if (flag == false)
+            if (time >= 0)
             {
-                //Debug.Log("x = " + this.transform.localPosition.x + " y = " + this.transform.localPosition.y + " z = " + this.transform.localPosition.z);
-                YValue = this.transform.localPosition.y - value;
-                this.transform.localPosition = new Vector3(0, YValue, 0);
-                count1 = determineCount(YValue);
+                time -= Time.deltaTime;
+                return;
+            }
+            else
+            {
 
-
-                for (int i = KE_Cubes.Length - 1; i > count1; i--)
+                if (flag == false)
                 {
-                    KE_Cubes[i].GetComponent<Renderer>().material.color = Color.green;
+                    YValue = this.transform.localPosition.y - value;
+                    this.transform.localPosition = new Vector3(0.14f, YValue, 0);
+
+
                 }
-                for (int i = 0; i < (PE_Cubes.Length - count1) - 1; i++)
+                else if (flag == true)
                 {
-                    PE_Cubes[i].GetComponent<Renderer>().material.color = Color.white;
+                    YValue = this.transform.localPosition.y + value;
+                    this.transform.localPosition = new Vector3(0.14f, YValue, 0);
+
+                    if (this.transform.localPosition.y >= ballheightupto)
+                    {
+                        flag = false;
+                    }
                 }
             }
-            if (flag == true)
+
+
+            velocity = ((transform.position - previous).magnitude) / Time.deltaTime;
+            previous = transform.position;
+
+
+            if (this.transform.localPosition.y <= 0.065)
             {
-                // Debug.Log("x = " + this.transform.localPosition.x + " y = " + this.transform.localPosition.y + " z = " + this.transform.localPosition.z);
-                YValue = this.transform.localPosition.y + value;
-                this.transform.localPosition = new Vector3(0, YValue, 0);
-
-                count2 = determineCount(YValue);
-
-                Debug.Log("count = " + count2);
-
-
-                for (int i = PE_Cubes.Length-1; i >= (PE_Cubes.Length - count2)-1 ; i--)
-                {
-                    Debug.Log("hello: " + i);
-                    PE_Cubes[i].GetComponent<Renderer>().material.color = Color.green;
-                }
-                for (int i = 0; i < count2; i++)
-                {
-                    KE_Cubes[i].GetComponent<Renderer>().material.color = Color.white;
-                }
-
-                if (this.transform.localPosition.y >= 0.7f)
-                {
-
-                    for (int i = 0; i < PE_Cubes.Length; i++)
-                    {
-                        PE_Cubes[i].GetComponent<Renderer>().material.color = Color.green;
-                    }
-                    for (int i = 0; i < KE_Cubes.Length; i++)
-                    {
-                        KE_Cubes[i].GetComponent<Renderer>().material.color = Color.white;
-                    }
-                    flag = false;
-                }
+               // Debug.Log("OHHHH");
+                height = 0;
             }
+            else
+            {
+               // Debug.Log("height:" + height);
+                height = this.transform.localPosition.y;
+            }
+
+            
+
+
+
+
+
+            KE = (0.5f) * (mass) * (velocity);
+            PE = mass * (gravitinal_force) * height;
+
+           // Debug.Log("---PE---:" + PE);
+
+
+
+            //kinetic_energy.text = "Kinetic Energy = " + KE.ToString("F2");
+            //potential_energy.text = "Potential Energy = " + PE.ToString("F2");
         }
-
-
-        velocity = ((transform.position - previous).magnitude) / Time.deltaTime;
-        previous = transform.position;
-
-        height = this.transform.localPosition.y;
-
-
-
-
-       
-        KE = (0.2f) * (mass) * (velocity);
-        PE = mass * (gravitinal_force) * height;
-
-
-
-        kinetic_energy.text = "Kinetic Energy = " + KE.ToString("F2");
-        potential_energy.text = "Potential Energy = " + PE.ToString("F2");
-    }
-    void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("---Collision---");
-        gravitinal_force = -9.8f;
-        for(int i = 0; i < KE_Cubes.Length; i++)
+        void OnCollisionEnter(Collision collision)
         {
-            KE_Cubes[i].GetComponent<Renderer>().material.color  = Color.green;
+           // Debug.Log("---Collision---:" + height);
+            flag = true;
         }
-        for (int i = 0; i < PE_Cubes.Length; i++)
-        {
-            PE_Cubes[i].GetComponent<Renderer>().material.color = Color.white;
-        }
-        count2 = 0;
-        flag = true;
-    }
+
+
     
-
 }
---
+++ Assets/Scripts/graph.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class graph : MonoBehaviour
+{
+
+    // Use this for initialization
+    public GameObject textObj;
+
+    Vector3 position;
+    Vector3 position_KE;
+    Vector3 position_PE;
+
+
+    int index = 1;
+
+    Vector3 scale = Vector3.one / 10f;
+
+    float delaypoints = 0.2f;
+    Boolean flag = false;
+
+
+    float gravForce = Ball.gravitinal_force;
+    float heightBall = Ball.height;
+    float massBall = Ball.mass;
+    int j = 0;
+
+
+
+    void Start()
+    {
+
+        position.x = -4.9f;
+        position.y = 0.0f;
+        //Debug.Log("---massBall is ---:" + massBall);
+        //Debug.Log("---gravForce is ---:" + gravForce);
+        //Debug.Log("---heightBall is ---:" + heightBall);
+        position.z = massBall * gravForce * heightBall * 0.01f;
+
+        if (position.z == 0.00)
+        {
+            //Debug.Log("---Z is zero ---:");
+            position.z = -4.2f;
+        }
+        else if (position.z > 0.00)
+        {
+            //Debug.Log("---Z is greater zero ---:");
+            position.z = -4.2f + position.z * 20f;
+        }
+        //Debug.Log("---Position Z is ---:" + position.z);
+
+
+
+        for (int i = 0; position.x <= 4.8; i++)
+        {
+            GameObject test = Instantiate(textObj);
+            test.GetComponent<Renderer>().material.color = Color.black;
+            position.x += 0.1f;
+
+            test.transform.localPosition = position;
+            test.transform.localScale = scale;
+            test.transform.SetParent(transform, false);
+        }
+
+        position_PE.z = position.z;
+        position_PE.x = -4.9f;
+        position_PE.y = 0.0f;
+
+
+        position_KE.z = -4.2f;
+        position_KE.x = -4.9f;
+        position_KE.y = 0.0f;
+
+
+    }
+
+
+    //Update is called once per frame
+    void Update()
+    {
+        if (j == 3)
+        {
+            return;
+        }
+        if (flag == false)
+        {
+            for (int i = 0; position_PE.z > -4.3f; i++)
+            {
+                GameObject test_PE = Instantiate(textObj);
+                test_PE.GetComponent<Renderer>().material.color = Color.blue;
+                position_PE.z -= 0.1f;
+
+                position_PE.x += (float)Math.Log(0.0001f * massBall * i * i * i * gravForce + 1, 1000000);
+
+                test_PE.transform.localPosition = position_PE;
+                test_PE.transform.localScale = scale;
+                test_PE.transform.SetParent(transform, false);
+
+            }
+
+
+            for (int i = 0; position_KE.z <= position.z; i++)
+            {
+                GameObject test_KE = Instantiate(textObj);
+                test_KE.GetComponent<Renderer>().material.color = Color.red;
+                position_KE.z += 0.1f;
+
+                position_KE.x += (float)Math.Log(0.0001f * massBall * i * i * i * gravForce + 1, 1000000);
+
+                test_KE.transform.localPosition = position_KE;
+                test_KE.transform.localScale = scale;
+                test_KE.transform.SetParent(transform, false);
+            }
+
+
+            flag = true;
+        }
+        else
+        {
+            for (int i = 0; position_PE.z <= position.z; i++)
+            {
+                GameObject test_PE = Instantiate(textObj);
+                test_PE.GetComponent<Renderer>().material.color = Color.blue;
+                position_PE.z += 0.1f;
+
+                position_PE.x += (float)Math.Log(0.0001f * massBall * i * i * i * gravForce + 1, 1000000);
+
+                test_PE.transform.localPosition = position_PE;
+                test_PE.transform.localScale = scale;
+                test_PE.transform.SetParent(transform, false);
+            }
+
+            //for (int i = 0; position_KE.z > -4.3f; i++)
+            //{
+            //    GameObject test_KE = Instantiate(textObj);
+            //    test_KE.GetComponent<Renderer>().material.color = Color.red;
+            //    position_KE.z -= 0.1f;
+
+            //    position_KE.x += (float)Math.Log(0.0001f * massBall * i * i * i * gravForce + 1, 1000000);
+
+            //    test_KE.transform.localPosition = position_KE;
+            //    test_KE.transform.localScale = scale;
+            //    test_KE.transform.SetParent(transform, false);
+            //}
+
+
+            flag = false;
+        }
+        j++;
+    }
+}
