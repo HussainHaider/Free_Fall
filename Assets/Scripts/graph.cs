c27bb43dc99c03127e3f2a2dbab4ac7df9434169
Hussain Haider <hussainhaider490@gmail.com>
2018-10-24 20:05:51 +0500

    Graph Completed

--- Assets/Scripts/graph.cs
+++ Assets/Scripts/graph.cs
@@ -17,14 +17,14 @@ public class graph : MonoBehaviour
 
     int index = 1;
 
-    Vector3 scale = Vector3.one / 10f;
+    Vector3 scale = Vector3.one / 7f;
 
     float delaypoints = 0.2f;
     Boolean flag = false;
 
 
     float gravForce = Ball.gravitinal_force;
-    float heightBall = Ball.height;
+    float heightBall =2;
     float massBall = Ball.mass;
     int j = 0;
 
@@ -43,12 +43,12 @@ public class graph : MonoBehaviour
         if (position.z == 0.00)
         {
             //Debug.Log("---Z is zero ---:");
-            position.z = -4.2f;
+            position.z = -4.5f;
         }
         else if (position.z > 0.00)
         {
             //Debug.Log("---Z is greater zero ---:");
-            position.z = -4.2f + position.z * 20f;
+            position.z = -4.5f + position.z * 20f;
         }
         //Debug.Log("---Position Z is ---:" + position.z);
 
@@ -70,7 +70,7 @@ public class graph : MonoBehaviour
         position_PE.y = 0.0f;
 
 
-        position_KE.z = -4.2f;
+        position_KE.z = -4.4f;
         position_KE.x = -4.9f;
         position_KE.y = 0.0f;
 
@@ -91,60 +91,82 @@ public class graph : MonoBehaviour
             {
                 GameObject test_PE = Instantiate(textObj);
                 test_PE.GetComponent<Renderer>().material.color = Color.blue;
-                position_PE.z -= 0.1f;
+                position_PE.x += 0.1f;
 
-                position_PE.x += (float)Math.Log(0.0001f * massBall * i * i * i * gravForce + 1, 1000000);
+                position_PE.z -= (float)Math.Log(0.01f * massBall * i * i * i * gravForce + 1, 1000000);
+
 
                 test_PE.transform.localPosition = position_PE;
                 test_PE.transform.localScale = scale;
                 test_PE.transform.SetParent(transform, false);
-
             }
 
+            GameObject test_PE_1 = Instantiate(textObj);
+            test_PE_1.GetComponent<Renderer>().material.color = Color.blue;
+            position_PE.x += 0.2f;
 
-            for (int i = 0; position_KE.z <= position.z; i++)
+            position_PE.z -= 0.2f;
+
+            test_PE_1.transform.localPosition = position_PE;
+            test_PE_1.transform.localScale = scale;
+            test_PE_1.transform.SetParent(transform, false);
+
+
+            for (int i = 0; position_KE.z < position.z; i++)
             {
                 GameObject test_KE = Instantiate(textObj);
                 test_KE.GetComponent<Renderer>().material.color = Color.red;
-                position_KE.z += 0.1f;
+                position_KE.x += 0.1f;
 
-                position_KE.x += (float)Math.Log(0.0001f * massBall * i * i * i * gravForce + 1, 1000000);
+                position_KE.z += (float)Math.Log(0.01f * massBall * i * i * i * gravForce + 1, 1000000);
+                Debug.Log("---Position Z is ---:" + position_KE.z);
 
                 test_KE.transform.localPosition = position_KE;
                 test_KE.transform.localScale = scale;
                 test_KE.transform.SetParent(transform, false);
             }
 
-
             flag = true;
         }
         else
         {
-            for (int i = 0; position_PE.z <= position.z; i++)
+            for (int i = 0; position_PE.z < position.z; i++)
             {
                 GameObject test_PE = Instantiate(textObj);
                 test_PE.GetComponent<Renderer>().material.color = Color.blue;
-                position_PE.z += 0.1f;
+                position_PE.x += 0.1f;
 
-                position_PE.x += (float)Math.Log(0.0001f * massBall * i * i * i * gravForce + 1, 1000000);
+                position_PE.z += (float)Math.Log(0.01f * massBall * i * i * i * gravForce + 1, 1000000);
+                Debug.Log("---Position Z is ---:" + position_PE.z);
 
                 test_PE.transform.localPosition = position_PE;
                 test_PE.transform.localScale = scale;
                 test_PE.transform.SetParent(transform, false);
             }
 
-            //for (int i = 0; position_KE.z > -4.3f; i++)
-            //{
-            //    GameObject test_KE = Instantiate(textObj);
-            //    test_KE.GetComponent<Renderer>().material.color = Color.red;
-            //    position_KE.z -= 0.1f;
+            for (int i = 0; position_KE.z > -4.3f; i++)
+            {
+                GameObject test_KE = Instantiate(textObj);
+                test_KE.GetComponent<Renderer>().material.color = Color.red;
+                position_KE.x += 0.1f;
 
-            //    position_KE.x += (float)Math.Log(0.0001f * massBall * i * i * i * gravForce + 1, 1000000);
+                position_KE.z -= (float)Math.Log(0.01f * massBall * i * i * i * gravForce + 1, 1000000);
 
-            //    test_KE.transform.localPosition = position_KE;
-            //    test_KE.transform.localScale = scale;
-            //    test_KE.transform.SetParent(transform, false);
-            //}
+                test_KE.transform.localPosition = position_KE;
+                test_KE.transform.localScale = scale;
+                test_KE.transform.SetParent(transform, false);
+            }
+
+            GameObject test_KE_1 = Instantiate(textObj);
+            test_KE_1.GetComponent<Renderer>().material.color = Color.red;
+            position_KE.x += 0.2f;
+
+            position_KE.z -= 0.2f;
+
+            test_KE_1.transform.localPosition = position_KE;
+            test_KE_1.transform.localScale = scale;
+            test_KE_1.transform.SetParent(transform, false);
+
 
 
             flag = false;
